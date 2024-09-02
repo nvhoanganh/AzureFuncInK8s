a9d12daf0800af3155bbcc31f0d037bda17c3488
Anthony Nguyen <nvhoanganh1909@gmail.com>
2024-09-02 21:59:47 +1000

    use App insights

--- Program.cs
+++ Program.cs
@@ -3,19 +3,41 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using AzureFuncInK8s;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 string connectionString = Environment.GetEnvironmentVariable("SqlConnection");
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services => {
+    .ConfigureServices(services =>
+    {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddLogging(loggingBuilder =>
+        {
+            loggingBuilder.AddApplicationInsights();
+            // Set the minimum logging level to Information
+            loggingBuilder.SetMinimumLevel(LogLevel.Information);
+        });
         services.AddDbContext<FuncDbContext>(
           options => SqlServerDbContextOptionsExtensions.UseSqlServer(options, connectionString, b =>
             {
                 // additional config goes here
             }));
+    })
+    // need to do this to show Information level logs
+    // https://learn.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide?tabs=windows#managing-log-levels
+    .ConfigureLogging(logging =>
+    {
+        logging.Services.Configure<LoggerFilterOptions>(options =>
+        {
+            LoggerFilterRule defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName
+                == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+            if (defaultRule is not null)
+            {
+                options.Rules.Remove(defaultRule);
+            }
+        });
     })
     .Build();
 
